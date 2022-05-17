17b1bf8d06dd6b9ad0c84313371bdd17057fc578
alemenew <alemenewendawek@gmail.com>
2022-05-17 15:47:43 +0300

    str functions

--- functions.c
+++ functions.c
@@ -1,122 +1,86 @@
 #include "main.h"
 
+int _strlen(const char *s);
+char *_strcpy(char *dest, const char *src);
+char *_strcat(char *dest, const char *src);
+char *_strncat(char *dest, const char *src, size_t n);
+
 /**
- * _strlen - find the length of a string
- * @s: pointer to the string to check
- * Return: void
-*/
-
-
-int _strlen(char *str)
+ * _strlen - Returns the length of a string.
+ * @s: A pointer to the characters string.
+ *
+ * Return: The length of the character string.
+ */
+int _strlen(const char *s)
 {
-int i = 0;
-while (str[i])
-	i++;
+	int length = 0;
 
-return (i);
+	if (!s)
+		return (length);
+	for (length = 0; s[length]; length++)
+		;
+	return (length);
 }
 
-
 /**
- * *_strcpy -  copies the string pointed to by src
- * @dest: char type string
- * @src: char type string
- * Description: Copy the string pointed to by pointer `src` to
- * the buffer pointed to by `dest`
- * Return: Pointer to `dest`
+ * _strcpy - Copies the string pointed to by src, including the
+ *           terminating null byte, to the buffer pointed by des.
+ * @dest: Pointer to the destination of copied string.
+ * @src: Pointer to the src of the source string.
+ *
+ * Return: Pointer to dest.
  */
-
 char *_strcpy(char *dest, const char *src)
 {
-	int i = -1;
+	size_t i;
 
-	do {
-		i++;
+	for (i = 0; src[i] != '\0'; i++)
 		dest[i] = src[i];
-	} while (src[i] != '\0');
-
+	dest[i] = '\0';
 	return (dest);
 }
+
 /**
- * _strdup - returns pointer to new mem alloc space which contains copy
- * @strtodup: string to be duplicated
- * Return: a pointer to the new duplicated string
+ * _strcat - Concantenates two strings.
+ * @dest: Pointer to destination string.
+ * @src: Pointer to source string.
+ *
+ * Return: Pointer to destination string.
  */
-char *_strdup(char *strtodup)
+char *_strcat(char *dest, const char *src)
 {
-	char *copy;
+	char *destTemp;
+	const char *srcTemp;
 
-	int len, i;
+	destTemp = dest;
+	srcTemp =  src;
 
-	if (strtodup == 0)
-		return (NULL);
+	while (*destTemp != '\0')
+		destTemp++;
 
-	for (len = 0; strtodup[len]; len++)
-		;
-	copy = malloc((len + 1) * sizeof(char));
-
-	for (i = 0; i <= len; i++)
-		copy[i] = strtodup[i];
-
-	return (copy);
+	while (*srcTemp != '\0')
+		*destTemp++ = *srcTemp++;
+	*destTemp = '\0';
+	return (dest);
 }
 
 /**
- * _strcmpr - compares two strings
- * @strcmp1: first string, of two, to be compared in length
- * @strcmp2: second string, of two, to be compared
- * Return: 0 on success, anything else is a failure
+ * _strncat - Concantenates two strings where n number
+ *            of bytes are copied from source.
+ * @dest: Pointer to destination string.
+ * @src: Pointer to source string.
+ * @n: n bytes to copy from src.
+ *
+ * Return: Pointer to destination string.
  */
-int _strcmpr(char *strcmp1, char *strcmp2)
+char *_strncat(char *dest, const char *src, size_t n)
 {
-	int i;
+	size_t dest_len = _strlen(dest);
+	size_t i;
 
-	i = 0;
-	while (strcmp1[i] == strcmp2[i])
-	{
-		if (strcmp1[i] == '\0')
-			return (0);
-		i++;
-	}
-	return (strcmp1[i] - strcmp2[i]);
-}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[dest_len + i] = src[i];
+	dest[dest_len + i] = '\0';
 
-/**
- * _strcat - concatenates two strings
- * @strc1: first string
- * @strc2: second string
- * Return: pointer
- */
-char *_strcat(char *strc1, char *strc2)
-{
-	char *newstring;
-	unsigned int len1, len2, newlen, i, j;
-
-	len1 = 0;
-	len2 = 0;
-	if (strc1 == NULL)
-		len1 = 0;
-	else
-	{
-		for (len1 = 0; strc1[len1]; len1++)
-			;
-	}
-	if (strc2 == NULL)
-		len2 = 0;
-	else
-	{
-		for (len2 = 0; strc2[len2]; len2++)
-			;
-	}
-	newlen = len1 + len2 + 2;
-	newstring = malloc(newlen * sizeof(char));
-	if (newstring == NULL)
-		return (NULL);
-	for (i = 0; i < len1; i++)
-		newstring[i] = strc1[i];
-	newstring[i] = '/';
-	for (j = 0; j < len2; j++)
-		newstring[i + 1 + j] = strc2[j];
-	newstring[len1 + len2 + 1] = '\0';
-	return (newstring);
+	return (dest);
 }
--- main.h
+++ main.h
@@ -1,60 +1,135 @@
 #ifndef SIMPLE_SHELL
 #define SIMPLE_SHELL
 
-#include <string.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <limits.h>
+#include <fcntl.h>
 #include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+
+#define END_OF_FILE -2
+#define EXIT -3
+
+/* Global environemnt */
+extern char **environ;
+/* Global program name */
+char *name;
+/* Global history counter */
+int hist;
 
 /**
- * struct variables - varables
- * @av: command line arguments
- * @buffer: buffer of command
- * @env: enviroment varables
- * @count: count of commands enterd
- * @argv: arguments at opening of shell
- * @status: exit status
- * @commands : commands
+ * struct list_s - A new struct type defining a linked list.
+ * @dir: A directory path.
+ * @next: A pointer to another struct list_s.
  */
-
-typedef struct variables
+typedef struct list_s
 {
-	char **av;
-	char *buffer;
-	char **env;
-	size_t count;
-	char **argv;
-	int status;
-	char **commands;
-} vars_t;
+	char *dir;
+	struct list_s *next;
+} list_t;
 
+/**
+ * struct builtin_s - A new struct type defining builtin commands.
+ * @name: The name of the builtin command.
+ * @f: A function pointer to the builtin command's function.
+ */
+typedef struct builtin_s
+{
+	char *name;
+	int (*f)(char **argv, char **front);
+} builtin_t;
 
-int _putchar(char c);
-void _puts(char *str);
-void _puts2(char *str);
-unsigned int count_spaces(char *s);
-char **_strtok(char *str);
+/**
+ * struct alias_s - A new struct defining aliases.
+ * @name: The name of the alias.
+ * @value: The value of the alias.
+ * @next: A pointer to another struct alias_s.
+ */
+typedef struct alias_s
+{
+	char *name;
+	char *value;
+	struct alias_s *next;
+} alias_t;
+
+/* Global aliases linked list */
+alias_t *aliases;
+
+/* Main Helpers */
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+char **_strtok(char *line, char *delim);
+char *get_location(char *command);
+list_t *get_path_dir(char *path);
+int execute(char **args, char **front);
+void free_list(list_t *head);
+char *_itoa(int num);
+
+/* Input Helpers */
+void handle_line(char **line, ssize_t read);
+void variable_replacement(char **args, int *exe_ret);
+char *get_args(char *line, int *exe_ret);
+int call_args(char **args, char **front, int *exe_ret);
+int run_args(char **args, char **front, int *exe_ret);
+int handle_args(int *exe_ret);
+int check_args(char **args);
+void free_args(char **args, char **front);
+char **replace_aliases(char **args);
+
+/* String functions */
+int _strlen(const char *s);
+char *_strcat(char *dest, const char *src);
+char *_strncat(char *dest, const char *src, size_t n);
 char *_strcpy(char *dest, const char *src);
-int execute(char **cmd);
-int main(int argc, char **argv);
-char **make_env(char **env);
-void free_env(char **env);
-int _strlen(char *str);
-char *_strcat(char *strc1, char *strc2);
-int _strcmpr(char *strcmp1, char *strcmp2);
-char *_strdup(char *strtodup);
-void print_error(vars_t *vars, char *msg);
-char *_uitoa(unsigned int count);
-int path_execute(char *command, vars_t *vars);
-char *find_path(char **env);
-void check_for_path(vars_t *vars);
-int execute_cwd(vars_t *vars);
-int check_for_dir(char *str);
+char *_strchr(char *s, char c);
+int _strspn(char *s, char *accept);
+int _strcmp(char *s1, char *s2);
+int _strncmp(const char *s1, const char *s2, size_t n);
 
+/* Builtins */
+int (*get_builtin(char *command))(char **args, char **front);
+int shellby_exit(char **args, char **front);
+int shellby_env(char **args, char __attribute__((__unused__)) **front);
+int shellby_setenv(char **args, char __attribute__((__unused__)) **front);
+int shellby_unsetenv(char **args, char __attribute__((__unused__)) **front);
+int shellby_cd(char **args, char __attribute__((__unused__)) **front);
+int shellby_alias(char **args, char __attribute__((__unused__)) **front);
+int shellby_help(char **args, char __attribute__((__unused__)) **front);
 
-#endif
+/* Builtin Helpers */
+char **_copyenv(void);
+void free_env(void);
+char **_getenv(const char *var);
+
+/* Error Handling */
+int create_error(char **args, int err);
+char *error_env(char **args);
+char *error_1(char **args);
+char *error_2_exit(char **args);
+char *error_2_cd(char **args);
+char *error_2_syntax(char **args);
+char *error_126(char **args);
+char *error_127(char **args);
+
+/* Linkedlist Helpers */
+alias_t *add_alias_end(alias_t **head, char *name, char *value);
+void free_alias_list(alias_t *head);
+list_t *add_node_end(list_t **head, char *dir);
+void free_list(list_t *head);
+
+void help_all(void);
+void help_alias(void);
+void help_cd(void);
+void help_exit(void);
+void help_help(void);
+void help_env(void);
+void help_setenv(void);
+void help_unsetenv(void);
+void help_history(void);
+
+int proc_file_commands(char *file_path, int *exe_ret);
+#endif /* _SHELL_H_ */
